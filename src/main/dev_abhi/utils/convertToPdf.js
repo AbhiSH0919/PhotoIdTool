b4fe2738f9fde9aedf18cc5e11df0915516bd56b
dev-Abhi0919 <kembalikarabhi19@gmail.com>
2025-06-01 21:21:04 +0530

    - Added lazy load in converToPdf function (load script only when download pdf) - Fixed css path in gitignore.

--- src/main/dev_abhi/utils/convertToPdf.js
+++ src/main/dev_abhi/utils/convertToPdf.js
@@ -1,19 +1,10 @@
-import html2pdf from "html2pdf.js";
+// import html2pdf from "html2pdf.js";
 
 const convertToPdf = async function (eleContent) {
+	if (!eleContent) return;
+
 	const pdfContent = eleContent;
 	pdfContent.classList.add("showLoader", "pdf-export");
-
-	const opt = {
-		position: "relative",
-		top: 0,
-		left: 0,
-		margin: 1,
-		filename: "Img.pdf",
-		image: { type: "png", quality: 1 },
-		html2canvas: { scale: 4, useCORS: true },
-		jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
-	};
 
 	const images = pdfContent.querySelectorAll(".img");
 	const promises = [];
@@ -31,11 +22,29 @@ const convertToPdf = async function (eleContent) {
 	});
 
 	try {
+		const html2pdf = (await import("html2pdf.js")).default;
+		const html2canvas = await import("html2canvas");
+
 		await Promise.all(promises);
+
+		const opt = {
+			position: "relative",
+			top: 0,
+			left: 0,
+			margin: 1,
+			filename: "Img.pdf",
+			image: { type: "png", quality: 1 },
+			html2canvas: { scale: 4, useCORS: true },
+			jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+		};
+
 		await html2pdf().set(opt).from(pdfContent).save();
 	} catch (error) {
 		console.error(error);
-		alert("Something went wrong while generating the PDF.\nPlease try again.");
+		alert(
+			"Something went wrong while generating the PDF.\nPlease try again.\nIf the same issue persists, reload the page."
+		);
+		pdfContent.classList.remove("showLoader", "pdf-export");
 	} finally {
 		setTimeout(() => {
 			pdfContent.classList.remove("showLoader", "pdf-export");
